819ad94daca157fc4a517cefa7e74bbf7f02751e
Romulo San <romulossan@gmail.com>
2018-06-04 09:37:09 -0300

    ok

---
+++ CanvasWorldCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasWorldCamera : MonoBehaviour {
+
+	public Canvas canvas;
+
+	private void Reset() {
+		canvas = GetComponent<Canvas>();
+	}
+
+	private void Start() {
+		GetWorldCamera();
+	}
+
+	public void GetWorldCamera(){
+		canvas.worldCamera = Camera.main;
+	}
+}
