807417e23cf03d988d071046a2f194bcbe7389c7
Romulo San <romulossan@gmail.com>
2018-06-26 22:42:52 -0300

    ok

--- MenuFlowchart/ConnectionPoint.cs
+++ MenuFlowchart/ConnectionPoint.cs
@@ -10,45 +10,54 @@ namespace UIFramework {
 
 		public Rect rect = new Rect(0, 0, 20f, 20f);
 		public ConnectionPointType type;
-		public GUIStyle style, labelStyle;
+		public GUISkin skin;
+		public GUIStyle labelStyle, textFieldstyle,buttonMinus;
+		public string textName="name";
 
 		public int id;
-		public Rect labelRect = new Rect(0,0,100,20);
+		public Rect labelRect = new Rect(0,0,60,20);
 
-		public ConnectionPoint(ConnectionPointType type, GUISkin skin){
+		public ConnectionPoint(ConnectionPointType type){
 			this.type = type;
-			this.style = skin.button;
-			labelStyle = skin.label;
 		}
 
-		public void Draw(Node node,int i){
+		public void Draw(Node node,int i,GUISkin skin){
 			id = i;
 			Rect label = labelRect;
 			switch (type){
 			case ConnectionPointType.In:
-				rect.y = node.rect.y + (node.rect.height * 0.5f) - rect.height * 0.5f;
+				if(node.menu.transitions.Count>1)
+					rect.y = node.rect.y + (node.rect.height * 0.5f) - rect.height * 0.5f;
+				else
+					rect.y = node.rect.y + 25;
 				rect.x = node.rect.x - rect.width + 8f;
 				label.x = rect.x;
 				label.y	= rect.y;
 				label.x += 20;
-				labelStyle.alignment = TextAnchor.MiddleLeft;
-				GUI.Label(label,"Enable",labelStyle);
-				if (GUI.Button(rect, "", style)){
+				skin.label.alignment = TextAnchor.MiddleLeft;
+				GUI.Label(label,"Open",skin.label);
+				if (GUI.Button(rect, "", skin.button)){
 					NodeEditor.ClickInPoint(this);
 				}
 				break;
 
 			case ConnectionPointType.Out:
 				if(id >= node.menu.transitions.Count) return;
-				rect.y = node.rect.y + 25 *(id+1);
+				rect.y = node.rect.y +5+ (25 *(id+1));
 				rect.x = node.rect.x + node.rect.width - 8f;
 				label.x = rect.x;
 				label.y	= rect.y;
 				label.x -= label.width;
-				labelStyle.alignment = TextAnchor.MiddleRight;
-				GUI.Label(label,node.menu.transitions[id].button.name,labelStyle);
-				if (GUI.Button(rect, "", style)){
+				skin.textField.alignment = TextAnchor.MiddleRight;
+				//GUI.Label(label,node.menu.transitions[id].button.name,labelStyle);
+
+				textName = GUI.TextField(label,textName, skin.textField);
+				if (GUI.Button(rect, "", skin.button)){
 					NodeEditor.ClickOutPoint(this);
+				}
+				float btnSize = 27f;
+				if (GUI.Button (new Rect (label.x-16,label.y+5,btnSize,btnSize), "-", skin.customStyles[2])) {
+					
 				}
 				break;
 			}
--- MenuFlowchart/Node.cs
+++ MenuFlowchart/Node.cs
@@ -17,8 +17,9 @@ namespace UIFramework {
 		public List<ConnectionPoint> outPoint = new List<ConnectionPoint>();
 		public GUIStyle style, defaultNodeStyle, selectedNodeStyle,styleShadow;
 		public GUISkin skin;
+		public string menuName="Menu";
 
-		private float width = 200;
+		private float width = 150;
 		private float height;
 
 		public Node(Menu menu, Vector2 position, GUISkin skin){
@@ -26,16 +27,15 @@ namespace UIFramework {
 
 			height = 50+(menu.transitions.Count-1)*25;
 			rect = new Rect(position.x, position.y, width, height);
-			inPoint = new ConnectionPoint(ConnectionPointType.In,  skin);
+			inPoint = new ConnectionPoint(ConnectionPointType.In);
 			for (int i = 0; i < menu.transitions.Count; i++) {
-				ConnectionPoint outP = new ConnectionPoint( ConnectionPointType.Out, skin);
+				ConnectionPoint outP = new ConnectionPoint( ConnectionPointType.Out);
 				outPoint.Add(outP);
 			}
 			style = defaultNodeStyle = skin.box;
 			selectedNodeStyle = skin.customStyles[0];
 			this.skin = skin;
 			styleShadow = skin.customStyles[1];
-
 		}
 
 		public bool ConnectionOutExists(ConnectionPoint con){
@@ -56,7 +56,7 @@ namespace UIFramework {
 				OnClickRemoveNode();
 				return;
 			}
-			height = 50+(menu.transitions.Count-1)*25;
+			height = 70+(menu.transitions.Count-1)*25;
 			rect.height = height;
 			//Color shadowCol = new Color(0, 0, 0, 0.06f);
 			//for (int i = 0; i < 3; i++){ // Draw a shadow
@@ -65,11 +65,13 @@ namespace UIFramework {
 			rectshadow.y+=4;
 			GUI.Box(rectshadow, "", styleShadow);
 			//}
-			GUI.Box(rect, menu.name, style);
-			inPoint.Draw(this,0);
+			GUI.Box(rect, "", style);
+			skin.textField.alignment = TextAnchor.MiddleCenter;
+			menuName = GUI.TextField(new Rect(rect.x+rect.width/2f-width/2f, rect.y, width, 20f),menuName, skin.textField);
+			inPoint.Draw(this,0,skin);
 			if(menu.transitions.Count > outPoint.Count){
 				for (int i = outPoint.Count-1; i < menu.transitions.Count; i++) {
-					ConnectionPoint outP = new ConnectionPoint(ConnectionPointType.Out, skin);
+					ConnectionPoint outP = new ConnectionPoint(ConnectionPointType.Out);
 					outPoint.Add(outP);
 				}
 			}else if(menu.transitions.Count < outPoint.Count){
@@ -79,7 +81,12 @@ namespace UIFramework {
 				}
 			}
 			for (int i = 0; i < outPoint.Count; i++) {
-				outPoint[i].Draw(this,i);
+				outPoint[i].Draw(this,i,skin);
+			}
+
+			float btnSize = 30f;
+			if (GUI.Button (new Rect (rect.x+rect.width-20,rect.y+rect.height-20,btnSize,btnSize), "+", skin.customStyles[2])) {
+				
 			}
 		}
 
